95758020e67bb5dcbef00c109e75e8662b2f19ec
ARIF <110717484+Arif1920002@users.noreply.github.com>
2023-05-01 21:15:05 +0600

    V1.0

--- Q7.c
+++ Q7.c
@@ -3,3 +3,22 @@ basic salary and house rent is 20% of his basic salary. Write a program to calcu
 gross salary.*/
 
 
+#include <stdio.h>
+
+void main()
+{
+    float salary, gross_salary, dearness_allowance, house_rent;
+    printf("Enter salary: \n");
+    scanf("%f", &salary);
+
+//calculating the salaries
+    dearness_allowance = salary*(40.0/100.0);
+    house_rent = salary*(20.0/100.0);
+    gross_salary = salary + house_rent + dearness_allowance;
+
+//printing the salaries
+    printf("dearness allowance= %f\n" , dearness_allowance);
+    printf("house rent= %f\n" , house_rent);
+    printf("gross salary = %f\n", gross_salary);
+
+}

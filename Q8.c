2d881d91bb445f49bfc0efd7520390a8d2f4748e
ARIF <110717484+Arif1920002@users.noreply.github.com>
2023-05-04 20:58:38 +0600

    v2

--- Q8.c
+++ Q8.c
@@ -5,3 +5,29 @@ Assume that the maximum marks that can be obtained by a student in each subject 
 
 
 
+#include <stdio.h>
+
+void main()
+{
+    float a,b,c,d,e,total,avg;
+    printf("Enter Numbers :\n");
+    scanf("%f %f %f %f %f", &a,&b,&c,&d,&e);
+
+    if (a >= 100 || b >= 100 || c >= 100 || d >= 100 || e >= 100)
+    {
+        printf("Maximum number can't exceed 100");
+    }
+
+    else
+    {
+        total = a+b+c+d+e;
+        avg = total / 5;
+
+            printf("Total marks = %f\n", total);
+            printf("Average marks = %f\n", avg);
+    }
+
+
+
+
+}
